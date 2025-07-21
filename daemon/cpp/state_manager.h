bce71798032cd68f85e79cdadf31ab47e58dad8a
zzaolv <2863436223@qq.com>
2025-07-21 01:03:29 -0700

    fix

--- daemon/cpp/state_manager.h
+++ daemon/cpp/state_manager.h
@@ -1,6 +1,7 @@
 // daemon/cpp/state_manager.h
 #ifndef CERBERUS_STATE_MANAGER_H
 #define CERBERUS_STATE_MANAGER_H
+
 #include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
@@ -14,16 +15,24 @@
 #include "action_executor.h"
 
 using json = nlohmann::json;
+
 struct AppRuntimeState {
-    enum class Status { STOPPED, RUNNING, FROZEN } current_status = Status::STOPPED;
+    enum class Status { 
+        STOPPED,
+        RUNNING,
+        FROZEN
+    } current_status = Status::STOPPED;
+
     std::string package_name;
     std::string app_name;
     int uid = -1;
     int user_id = 0;
     std::vector<int> pids; 
     AppConfig config;
+    
     bool is_foreground = false;
     time_t background_since = 0;
+
     float cpu_usage_percent = 0.0f;
     long mem_usage_kb = 0;
     long swap_usage_kb = 0;
@@ -45,12 +54,21 @@ private:
     std::string get_package_name_from_pid(int pid, int& uid, int& user_id);
     void add_pid_to_app(int pid, const std::string&, int user_id, int uid);
     void remove_pid_from_app(int pid);
+    
     AppRuntimeState* get_or_create_app_state(const std::string&, int user_id);
     bool is_critical_system_app(const std::string&) const;
+
+    // [修复] 将这些被意外删除的成员变量声明加回来
+    std::shared_ptr<DatabaseManager> db_manager_;
+    std::shared_ptr<SystemMonitor> sys_monitor_;
+    std::shared_ptr<ActionExecutor> action_executor_;
+
     std::mutex state_mutex_;
     std::set<int> last_known_top_pids_;
+    
     GlobalStatsData global_stats_;
     FreezeMethod default_freeze_method_ = FreezeMethod::METHOD_SIGSTOP;
+    
     using AppInstanceKey = std::pair<std::string, int>; 
     std::map<AppInstanceKey, AppRuntimeState> managed_apps_;
     std::map<int, AppRuntimeState*> pid_to_app_map_;
