6f6cdf59cad5d73ff7b370a7f5e909e8a15b66c7
zzaolv <2863436223@qq.com>
2025-08-01 19:57:25 -0700

    fix

--- daemon/cpp/action_executor.cpp
+++ daemon/cpp/action_executor.cpp
@@ -19,6 +19,7 @@
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 namespace fs = std::filesystem;
 
